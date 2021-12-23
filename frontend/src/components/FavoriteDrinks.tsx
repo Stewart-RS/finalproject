81aa3bc9a51bb2d632e61723cb45628f4953fef0
Gina Stewart <stewart_rs@yahoo.com>
2021-12-23 16:09:03 -0500

    updated fav drinks page

--- frontend/src/components/FavoriteDrinks.tsx
+++ frontend/src/components/FavoriteDrinks.tsx
@@ -8,31 +8,41 @@ export default function FavoritesList() {
     useContext(Favorites);
   console.log(favoritesList);
   return (
-    <div>
-      <h2 id="pagetitle">Your Favorite Drinks</h2>
-      <p id="content">Below are your favorite drinks. If you would like to favorite a new drink go to the bar.</p>
-      <div>
-        {favoritesList.map((favorite, index) => (
-          <div key={index}>
-            <a
-              target="_blank"
-              href={"https://www.thecocktaildb.com/drink/" + favorite.idDrink}
-              rel="noreferrer"
-            >
-              <img src={favorite.strDrinkThumb + "/preview"} alt="drink" />
-            </a>
-            <h3>{favorite.strDrink}</h3>
-            
-            <button id="removebutton"
-              onClick={() => {
-                removeFromFavorites(favorite.idDrink);
-              }}
-            >
-              Remove
-            </button>
+    <>
+      <section id="favStyling">
+        <div>
+          <h2 id="favPageTitle">Your Favorite Drinks</h2>
+          <p id="content">
+            Below are your favorite drinks. If you would like to favorite a new
+            drink go to the bar.
+          </p>
+          <div>
+            {favoritesList.map((favorite, index) => (
+              <div key={index}>
+                <a
+                  target="_blank"
+                  href={
+                    "https://www.thecocktaildb.com/drink/" + favorite.idDrink
+                  }
+                  rel="noreferrer"
+                >
+                  <img src={favorite.strDrinkThumb + "/preview"} alt="drink" />
+                </a>
+                <h3>{favorite.strDrink}</h3>
+
+                <button
+                  id="removebutton"
+                  onClick={() => {
+                    removeFromFavorites(favorite.idDrink);
+                  }}
+                >
+                  Remove
+                </button>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      </section>
+    </>
   );
 }
